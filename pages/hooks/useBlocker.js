0000362d0addd49e1ef3552969a62271be7f4824
kanishka-commits <kanishka03p@gmail.com>
2025-07-31 13:34:19 +0530

    Hooks for Navigation

---
+++ pages/hooks/useBlocker.js
@@ -0,0 +1,24 @@
+// hooks/useBlocker.js
+import { useContext, useEffect } from "react";
+import { NavigationContext } from "react-router-dom";
+
+export function useBlocker(blocker, when = true) {
+  const navigator = useContext(NavigationContext);
+
+  useEffect(() => {
+    if (!when) return;
+
+    const unblock = navigator.block((tx) => {
+      const autoUnblockingTx = {
+        ...tx,
+        retry() {
+          unblock();
+          tx.retry();
+        },
+      };
+      blocker(autoUnblockingTx);
+    });
+
+    return unblock;
+  }, [navigator, blocker, when]);
+}
